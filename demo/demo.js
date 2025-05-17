dec6e2a6a42e02eed4901ef479eaeae24fe0d1e9
SaribJawad <msssarib12@gmail.com>
2025-05-17 12:15:42 +0500

    refactor tabs to spaces

--- demo/demo.js
+++ demo/demo.js
@@ -11,12 +11,12 @@ const sub_title = 'Please fill out the form'
 
 const page = document.createElement('div')
 page.innerHTML = `
-		<h1>${title}</h1>
-		<h2>${sub_title}</h2>
-		<h3>Enter your age</h3>
-		<x></x>   
-		<h3>Enter your age of birth</h3>
-		<y></y>   
+    <h1>${title}</h1>
+    <h2>${sub_title}</h2>
+    <h3>Enter your age</h3>
+    <x></x>   
+    <h3>Enter your age of birth</h3>
+    <y></y>   
 `
 page.querySelector('x').replaceWith(input1)
 page.querySelector('y').replaceWith(input2)
